36667fe2a621beae4f9b443fa9f8df00b114b5d9
procopio420 <lucas-procopio@outlook.com>
2020-07-15 21:34:19 -0300

    cc

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,6 +1,38 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+function RankingTable(props) {
+  const { ranking } = props;
+
+  if (!ranking.length) return <h5 className="text-center">Infelizmente o ranking está vazio :(</h5>;
+
+  return (
+    <ul className="list-group">
+      {ranking
+        .sort((a, b) => a.score - b.score)
+        .map((player, index) => (
+          <li className="list-group-item">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-center ">
+                <img className="mr-2 rounded-circle" height="50px" src={player.picture} alt="" />
+                <p data-testid={`player-name-${index}`}>{player.name}</p>
+              </div>
+              <div className="d-flex align-items-center">
+                <small>Pontos: </small>
+                <span
+                  data-testid={`player-score-${index}`}
+                  className="badge badge-success badge-pill ml-2"
+                >
+                  {player.score}
+                </span>
+              </div>
+            </div>
+          </li>
+        ))}
+    </ul>
+  );
+}
+
 export default function Ranking() {
   const [ranking] = useState(() => {
     const rankingLocalStorage = localStorage.getItem('@trivia-game/ranking');
@@ -8,9 +40,6 @@ export default function Ranking() {
     return [];
   });
 
-  const handleBackButton = () => {
-    //reseta o score e assertions do player
-  };
   return (
     <div className="container">
       <div className="row justify-content-center align-items-center">
@@ -18,40 +47,11 @@ export default function Ranking() {
           <div className="card">
             <h1 className="card-title text-center">Ranking</h1>
             <div className="card-body">
-              <ul className="list-group">
-                {ranking.length ? (
-                  ranking
-                    .sort((a, b) => a.score - b.score)
-                    .map((player, index) => (
-                      <li className="list-group-item d-flex justify-content-between align-items-center">
-                        <div className="d-flex align-items-center ">
-                          <img
-                            className="mr-2 rounded-circle"
-                            height="50px"
-                            src={player.picture}
-                            alt=""
-                          />
-                          <p data-testid={`player-name-${index}`}>{player.name}</p>
-                        </div>
-                        <div className="d-flex align-items-center">
-                          <small>Pontos: </small>
-                          <span
-                            data-testid={`player-score-${index}`}
-                            className="badge badge-success badge-pill ml-2"
-                          >
-                            {player.score}
-                          </span>
-                        </div>
-                      </li>
-                    ))
-                ) : (
-                  <h5 className="text-center">Infelizmente o ranking está vazio :(</h5>
-                )}
-              </ul>
+              <RankingTable ranking={ranking} />
               <div className="d-flex justify-content-center mt-2">
                 <Link
                   to="/"
-                  onClick={handleBackButton}
+                  onClick={() => console.log('reseta o score e assertions do player')}
                   data-testid="btn-go-home"
                   className="btn btn-danger"
                 >

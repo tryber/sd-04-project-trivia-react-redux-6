5a538061d24d8ac81211d7835d00fbe0c84ac979
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-07-17 17:44:53 -0300

    cc issues

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Header from '../components/Header';
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function Feedback() {
-  const state = localStorage.getItem('state');
+  const state = localStorage.getItem("state");
   const user = JSON.parse(state).player;
   console.log(user);
 
   const messageFeedback = (questions) => {
-    if (questions => 3) {
-      return <h1 data-testid="feedback-test">Mandou Bem!</h1>
-    } else if (questions < 3) {
-      return <h1 data-testid="feedback-test">Podia ser melhor...</h1>
-    } 
+    if (questions >= 3) {
+      return <h1 data-testid="feedback-test">Mandou Bem!</h1>;
+    } else {
+      return <h1 data-testid="feedback-test">Podia ser melhor...</h1>;
+    }
   }
 
   return (
@@ -20,15 +20,31 @@ export default function Feedback() {
       <div className="row justify-content-center align-items-center">
         <div className="col">
           <div className="card">
-            <Header player={user}/>
-            <div className="container" >
+            <Header player={user} />
+            <div className="container">
               <div className="col my-5 align-items-center">
                 {messageFeedback(user.assertions)}
-                <h3 data-testid="feedback-total-question">Você acertou {user.assertions} questões</h3>
-                <h3 data-testid="feedback-total-score">Um total de {user.score} pontos</h3>
+                <h3 data-testid="feedback-total-question">
+                  Você acertou {user.assertions} questões
+                </h3>
+                <h3 data-testid="feedback-total-score">
+                  Um total de {user.score} pontos
+                </h3>
               </div>
-              <Link to="/ranking" data-testid="btn-ranking" className="btn btn-primary btn-block mt-5 mx-3">Ver Ranking</Link>
-              <Link to="/" data-testid="btn-play-again" className="btn btn-primary btn-block mt-5 mx-3">Jogar Novamente</Link>
+              <Link
+                to="/ranking"
+                data-testid="btn-ranking"
+                className="btn btn-primary btn-block mt-5 mx-3"
+              >
+                Ver Ranking
+              </Link>
+              <Link
+                to="/"
+                data-testid="btn-play-again"
+                className="btn btn-primary btn-block mt-5 mx-3"
+              >
+                Jogar Novamente
+              </Link>
             </div>
           </div>
         </div>
